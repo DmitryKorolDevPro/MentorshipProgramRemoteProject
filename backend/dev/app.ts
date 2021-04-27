b04a805710ed8e49b90fa7cc5d3dac9fd7b5fad0
Dmytro <blackdeer12@gmail.com>
2021-04-27 18:06:25 +0300

    Added CORS support.

--- backend/dev/app.ts
+++ backend/dev/app.ts
@@ -1,9 +1,11 @@
-import express from "express";
+import cors from 'cors';
+import express from 'express';
 import { router } from './router.js';
 
 const app = express();
-let port:number = process.env.PORT === undefined ? 4200 : +process.env.PORT;
+let port:number = process.env.PORT === undefined ? 5500 : +process.env.PORT;
 
+app.use(cors())
 app.use('/', router);
 
 app.listen(port, () => {
